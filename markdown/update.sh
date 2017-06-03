bfeb0d7f5a914f0450b9516916c9cb0b2bce8750
Ryan McCormick <rmccorm4@binghamton.edu>
2017-06-03 00:35:30 -0500

    update

--- markdown/update.sh
+++
@@ -1,5 +0,0 @@
-markdown2 main_page.md > ../index.html
-cd ..
-git add .
-git commit -m "update"
-git push
---
+++ update.sh
@@ -0,0 +1,4 @@
+markdown2 main_page.md > index.html
+git add .
+git commit -m "update"
+git push
